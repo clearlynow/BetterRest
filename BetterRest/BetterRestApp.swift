09b2514c0106b6787df450fbfee4bbbcc1829ed3
clearlynow <59779189+clearlynow@users.noreply.github.com>
2021-01-06 10:29:33 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Alison Gorman on 1/6/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
